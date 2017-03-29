d545320a6ad1513057513b758da16e2169887b8c
Daniel Makover <dmakover@seekingalpha.com>
2017-03-29 11:13:57 +0300

    spanish tranlation added

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ angular.module("translator", [])
 .controller("trnsController",function($http, $scope){
     var app = this;
     app.sendToAPI = function(input){
-      console.log(input);
-      //API CALL to Yandex
-      //$http.get()
-      $scope.output = input
-    }
+      var key ="trnsl.1.1.20170329T050632Z.750000a4f772eb56.f0e4b9018a16a51edd44edb6107ddc04869cc9c0";
+      var baseURL = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=";
 
+      //GET request to Yandex API
+      $http.get(baseURL + key + "&lang=en-es&text="+input)
+      .then(function(response){
+          $scope.output = response.data.text[0];
+        });
+      }
 });
